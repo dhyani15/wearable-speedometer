d5660b2931ff663fcb23a3f13ccd1431a1782dae
Naren Dhyani <31188643+dhyani15@users.noreply.github.com>
2021-12-21 17:48:43 -0500

    print float and detect pitstop

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,69 @@
 
 SPI spi(PF_9, PF_8, PF_7); // mosi, miso, sclk
 DigitalOut gyro_cs(PC_1);  // chip select
-
+int window_size =10;
+int pit_stop =0;
 int i = 0;
 bool flag = false;
 const int sample = 10;
 int sampling_rate = 0.5;
-int dps_x =0;
-int dps_y =0;
-int dps_z = 0;
+float dps_x =0;
+float dps_y =0;
+float dps_z = 0;
 float dx = 0;
 float dy = 0;
 float dz = 0;
+float noisy_x[100];
+float noisy_y[100];
+float noisy_z[100];
+int k=0;
 
 
+void display()
+{
+  printf("\n\r\n\r");
+  printf("datax = [");
+  for (int i=0;i<100;i++){
+    printf("%.2f,", noisy_x[i]);
+  }
+  printf("]");
+  printf("\n\r\n\r");
+  printf("datay = [");
+  for (int i=0;i<100;i++){
+      printf("%.2f,", noisy_y[i]);
+  }
+  printf("]");
+  printf("\n\r\n\r");
+  printf("dataz= [");
+  for (int i=0;i<100;i++){
+      printf("%.2f,", noisy_z[i]);
+  }
+  printf("]\n");
+}
+int window_sum(int start){
+    int sum=0;
+    for(int i=start; i<start+window_size; i++)
+    {
+      sum+=abs(noisy_x[i]);
+    }
+    return sum;
+  }
+void find_pitstop(){
+  bool f= false;
+  for (int i = 0;i<100;i+=10){
+    int sum1 = window_sum(i);
+    float avg= sum1/100;
+    if(avg<5)
+    {
+      f=true;
+      pit_stop=i;
+    }
+  }
+  if(f==false)
+  {
+    pit_stop=100;
+  }
+}
 void get_sample(){
     for (int j=0;j<sample;j++){
       gyro_cs = 0;
@@ -50,7 +100,10 @@ void get_sample(){
       short z_axis_low = spi.write(0x00);
       short z_axis_high = spi.write(0x00);
       short z_axis = z_axis_high << 8 | z_axis_low;
-
+      noisy_x[k]=x_axis*0.00875; // degrees per second
+      noisy_y[k]=y_axis*0.00875;
+      noisy_z[k]=z_axis*0.00875;
+      k++;
       gyro_cs = 1;
       // degrees per second
       dps_x += abs((x_axis *0.00875)); 
@@ -86,14 +139,14 @@ void config_gyro(){
 int main()
 {
   config_gyro();
-  int avg_dps_x[sample*2];
-  int avg_dps_y[sample*2];
-  int avg_dps_z[sample*2];
+  float avg_dps_x[sample];
+  float avg_dps_y[sample];
+  float avg_dps_z[sample];
   while (1){
     printf("New batch\n");
     wait_us(1000000);
     
-    for (int i=0;i<sample*2;i++){ // 5 seconds to run
+    for (int i=0;i<sample;i++){ // 5 seconds to run
       get_sample(); // 0.5 seconds to run
       avg_dps_x[i] = dps_x/sample;
       avg_dps_y[i] = dps_y/sample;
@@ -102,20 +155,30 @@ int main()
       dps_y = 0;
       dps_z = 0;
     }
-    
-    
-    for (int j=0;j<sample*2;j++){
+    k=0;
+    display();
+    find_pitstop();
+  
+    for (int j=0;j<sample;j++){
       dx += avg_dps_x[j] * 0.5 * 0.0174533;
       dy += avg_dps_y[j] * 0.5 * 0.0174533;
       dz += avg_dps_z[j] * 0.5 * 0.0174533;
     }
 
     float dis = sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
-    int s = dis * 1.5;
-    printf("%d\n", s);
-    avg_dps_x[sample*2] = {0};
-    avg_dps_y[sample*2] = {0};
-    avg_dps_z[sample*2] = {0};
+    float s = dis * 1.5;
+    if(s<0.5)
+    {
+      s=0.00;  //This is when there is no movement, so it becomes 0/0 
+    }
+    //float time=((pit_stop*0.5)/10);
+    float linear_velocity = s/((pit_stop*0.5)/10);
+    printf("distance  = %.2f\n", s);
+    //printf("Walked for = %.2f\n", time);
+    printf("velocity = %.2f\n", linear_velocity);
+    avg_dps_x[sample] = {0};
+    avg_dps_y[sample] = {0};
+    avg_dps_z[sample] = {0};
     dx=0;
     dy=0;
     dz=0;
