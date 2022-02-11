d11855007cb28fcd4f139f93b809e468fd2bee6d
Naren Dhyani <31188643+dhyani15@users.noreply.github.com>
2022-02-10 22:32:45 -0500

    make final changes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # wearable-speedometer
-Codebase to use gyro sensor to calculate linear velocity. Made using Mbed on PlatformIO
+Measuring self selected walking speed is useful in various clinical applications of gait analysis and helps medical practitioners to measure progress of a patient and recommend new therapy based on this data.
+
+I have created an embedded application on STM32 Discovery microcontroller to develop a wearable device that can measure the distance and walking speed using only the gyroscope sensor data.
+
+## Algorithm
+I modeled each leg as one single segment, and the two legs are assumed to
+be of the same length; thus, while walking when the heel-strikes, the two legs and the distance between the feet formed an isosceles triangle.
+The angle between the leg segments was calculated by integrating the angular velocity measurement from the gyroscope attached to the shank, and then the distance between two feet (step length, one half of stride length) was calculated using the properties of the arc length that subtends an angle.
+The figure below illustrates the ideas involved in finding the stride length.
+
+## Setup
+Setup [PlatformIO](https://platformio.org/) in your Visual Studio Code IDE and connect the [STM board](https://www.st.com/en/evaluation-tools/32f429idiscovery.html) to your computer. Download the repository in your local project folder and compile it to the microcontroller. Once you have compiled it, the lcd display on the board will start highlighting the output. Wear the device near the ankle; start walking and the device will highlight the distance walked every 20 seconds.
+
+## Unique features
+I have setup the device in such a way that it will detect the resting phases and walking phases during the 20 second measuring interval and takes that into account when calculating your walking speed and distance travelled.
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "mbed.h"
 #include "math.h"
 #include "LCD_DISCO_F429ZI.h"
-
 // setup the SPI
 // PC1 - CS (chip select for gyro)
 // PF7 - SCLK (Serial clock)
@@ -14,47 +13,44 @@ DigitalOut gyro_cs(PC_1);  // chip select
 int window_size =3;
 float walking_time =0;
 int i = 0;
-bool flag = false;
 const int sample = 10;
-int sampling_rate = 0.5;
+// stores the degree per second values of x,y and z axis for each sample
 float dps_x =0;
 float dps_y =0;
 float dps_z = 0;
+// stores the running sume of degrees over the interval of 20s
 float dx = 0;
 float dy = 0;
 float dz = 0;
-float noisy_x[100];
-float noisy_y[100];
-float noisy_z[100];
+// noisy variables stores the instantaneous raw values
+float noisy_x[400] = {0};
+float noisy_y[400] = {0};
+float noisy_z[400] = {0};
+float noisy_mag[400]={ 0};
 int k=0;
-float noisy_mag[100]={0};
+int cnt=0; // counter to keep track of number of times i run my get_sample() before printing distance
+float s =0; // variable to store distance
+float t=0; // running sum of total walking time
+float linear_velocity=0;
+// variables to store values of angular velocity after low pass filter
+
+// variables used to display text on lcd
 uint8_t text[30];
 uint8_t text1[30];
 uint8_t text2[30];
 
 
-void display()
+void display() // function to display raw data from the gyro
 {
   printf("\n\r\n\r");
-  printf("datax = [");
+  printf("data = [");
   for (int i=0;i<100;i++){
-    printf("%.2f,", noisy_x[i]);
-  }
-  printf("]");
-  printf("\n\r\n\r");
-  printf("datay = [");
-  for (int i=0;i<100;i++){
-      printf("%.2f,", noisy_y[i]);
-  }
-  printf("]");
-  printf("\n\r\n\r");
-  printf("dataz= [");
-  for (int i=0;i<100;i++){
-      printf("%.2f,", noisy_z[i]);
+    printf("%.2f,", noisy_mag[i]);
   }
   printf("]\n");
 }
-int window_sum(int start){
+
+int window_sum(int start){ // helper function that calculates average of 3 gyro samples
     int sum=0;
     for(int i=start; i<start+window_size; i++)
     {
@@ -63,7 +59,7 @@ int window_sum(int start){
     return sum;
   }
 
-void find_pitstop(){
+void find_walkingtime(){ // algorithm to calculate the total walking time for a sample
   
   float counter =0;
   for (int i =0;i<100;i++){
@@ -73,16 +69,48 @@ void find_pitstop(){
   for (int i = 0;i<98;i+=3){
     int sum1 = window_sum(i);
     float avg= sum1/3;
-    if(avg<50)
-    {
+    if(avg<25) {
       counter+=3;
     }
     walking_time = 5 - counter*0.05;
+    }
+}
 
+
+void init_LCD(){ // helper function to initialize the lcd
+    BSP_LCD_SetFont(&Font20);
+  lcd.Clear(LCD_COLOR_RED);
+  lcd.SetBackColor(LCD_COLOR_RED);
+  lcd.SetTextColor(LCD_COLOR_WHITE);
 }
+
+
+void set_LCD_red() // change the background color of lcd to red and print message
+{
+ lcd.Clear(LCD_COLOR_RED);
+      lcd.SetBackColor(LCD_COLOR_RED);
+      lcd.DisplayStringAt(0, LINE(7), (uint8_t *) "Calculating", CENTER_MODE); 
 }
-void get_sample(){
-    for (int j=0;j<sample;j++){
+
+void set_LCD_green(){ // change the background color of lcd to green and print message
+  lcd.SetBackColor(LCD_COLOR_GREEN);
+    lcd.DisplayStringAt(0, LINE(7), (uint8_t *) "  Sampling...", CENTER_MODE);
+}
+
+
+void lcd_display() // print the distance, velocity and total walking time over 20s to lcd
+{
+    sprintf((char*)text, "Distance = %.2f", s);
+    lcd.DisplayStringAt(0, LINE(9), (uint8_t *)&text, CENTER_MODE);
+    sprintf((char*)text2, "Walk Time = %.2f", t);
+    lcd.DisplayStringAt(0, LINE(11), (uint8_t *)&text2, CENTER_MODE);
+    sprintf((char*)text1, "Speed = %.2f", linear_velocity);
+    lcd.DisplayStringAt(0, LINE(13), (uint8_t *)&text1, CENTER_MODE);
+    
+}
+
+void get_sample(){ // get 10 gyro values with interval between each sample as 0.05s
+    for (int j=0;j<10;j++){
       gyro_cs = 0;
       spi.write(232);
       short x_axis_low = spi.write(0x00);
@@ -109,13 +137,15 @@ void get_sample(){
       short z_axis_low = spi.write(0x00);
       short z_axis_high = spi.write(0x00);
       short z_axis = z_axis_high << 8 | z_axis_low;
+      gyro_cs = 1;
+      // store values of sampled data
       noisy_x[k]=x_axis*0.00875; // degrees per second
       noisy_y[k]=y_axis*0.00875;
       noisy_z[k]=z_axis*0.00875;
       k++;
-      gyro_cs = 1;
+      
       // degrees per second
-      dps_x += abs((x_axis *0.00875)); 
+      dps_x += abs((x_axis *0.00875)); // multiply by 0.00875 to convert raw values to dps
       dps_y += abs((y_axis *0.00875));
       dps_z += abs((z_axis *0.00875));
       wait_us(50000);
@@ -123,7 +153,7 @@ void get_sample(){
 
 };
 
-void config_gyro(){
+void config_gyro(){ // function to wake the gyro so that it starts detecting values around x,y, and z axis
   // Chip must be deselected
   gyro_cs = 1;
 
@@ -145,65 +175,74 @@ void config_gyro(){
   wait_us(2000000);
   // check if there is no bit in one of the registers where it shows my data is not ready
 }
+
+
+
+
+// main logic of device
 int main()
 {
-  BSP_LCD_SetFont(&Font20);
-  lcd.Clear(LCD_COLOR_BLUE);
-  lcd.SetBackColor(LCD_COLOR_BLUE);
-  lcd.SetTextColor(LCD_COLOR_WHITE);
-  config_gyro();
   float avg_dps_x[sample];
-  float avg_dps_y[sample];
-  float avg_dps_z[sample];
-  while (1){
-    printf("New batch\n");
-    lcd.DisplayStringAt(0, LINE(7), (uint8_t *) "  Sampling...", CENTER_MODE);
-    wait_us(1000000);
+float avg_dps_y[sample];
+float avg_dps_z[sample];
+  init_LCD(); // set up lcd
+  config_gyro(); // set up gyro
+  
+
+  while (1){ // infinite loop
     
+    set_LCD_green();
+    wait_us(1000000);
+  
+    cnt=cnt+1;
     for (int i=0;i<sample;i++){ // 5 seconds to run
       get_sample(); // 0.5 seconds to run
-      avg_dps_x[i] = dps_x/sample;
-      avg_dps_y[i] = dps_y/sample;
-      avg_dps_z[i] = dps_z/sample;
+      avg_dps_x[i] = dps_x/10;
+      avg_dps_y[i] = dps_y/10;
+      avg_dps_z[i] = dps_z/10;
       dps_x = 0;
       dps_y = 0;
       dps_z = 0;
     }
-    lcd.DisplayStringAt(0, LINE(7), (uint8_t *) "Calculating", CENTER_MODE);
-    wait_us(1000000);
-    k=0;
-    //display();
-    find_pitstop();
-  
+    find_walkingtime();
+    // integrating step, angular velocity over 0.5s 
     for (int j=0;j<sample;j++){
-      dx += avg_dps_x[j] * 0.5 * 0.0174533;
+      dx += avg_dps_x[j] * 0.5 * 0.0174533; // multiply by 0.0174533 to convert it into radians
       dy += avg_dps_y[j] * 0.5 * 0.0174533;
       dz += avg_dps_z[j] * 0.5 * 0.0174533;
     }
 
-    float dis = sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2));
-    float s = dis * 1.5;
-    if(s<0.5)
-    {
-      s=0.00;  //This is when there is no movement, so it becomes 0/0 
-    }
-    //float time=((pit_stop*0.5)/10);
-    
-    float linear_velocity = s/(walking_time);
-    sprintf((char*)text, "Distance = %.2f", s);
-    lcd.DisplayStringAt(0, LINE(9), (uint8_t *)&text, CENTER_MODE);
-    sprintf((char*)text2, "Walk Time = %.2f", walking_time);
-    lcd.DisplayStringAt(0, LINE(11), (uint8_t *)&text2, CENTER_MODE);
-    sprintf((char*)text1, "Speed = %.2f", linear_velocity);
-    lcd.DisplayStringAt(0, LINE(13), (uint8_t *)&text1, CENTER_MODE);
-    printf("distance  = %.2f\n", s);
-    //printf("Walked for = %.2f\n", time);
-    printf("velocity = %.2f\n", linear_velocity);
+    float dis = sqrt(pow(dx, 2) + pow(dy, 2) + pow(dz, 2)); // finding the magnitude of x+y+z
+    s= s+ (dis * 1.5); // running sum of distance over 5s samples
+    t=t+walking_time; // running sum of walking time over 5s samples
+
+    // setting the values to zero for next batch of 5s samples
     avg_dps_x[sample] = {0};
     avg_dps_y[sample] = {0};
     avg_dps_z[sample] = {0};
     dx=0;
     dy=0;
     dz=0;
+
+    if(cnt>3) // after running 4 samples i.e. 20s of sampling, do processing and print values to lcd
+    {
+      //display(); uncomment me to print the raw values to serial terminal
+      k=0;
+      cnt=0;
+      set_LCD_red();
+      wait_us(1000000);
+
+      if(s<0.5)
+      {
+        s=0.00;  //This is when there is no movement, so it becomes 0/0 
+      }
+      
+      linear_velocity = s/(t); // calculating linear velocity
+      printf("%0.2f",s);
+      lcd_display(); // display values to lcd
+      s=0;
+      t=0;
+    }
+    
   }
 }
